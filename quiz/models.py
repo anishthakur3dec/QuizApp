a4abec1312c8b04cc6bc3983cedbc83e619852ad
Anish Kumar Thakur <anishthakur3dec@gmail.com>
2024-01-24 20:40:21 +0530

    Changes to be committed: 	modified:   account/__pycache__/views.cpython-311.pyc 	modified:   account/views.py 	modified:   db.sqlite3 	modified:   finalSubmission/__pycache__/urls.cpython-311.pyc 	modified:   finalSubmission/urls.py 	new file:   media/profile_images/Capture.PNG 	new file:   media/profile_images/mjOX2D.jpg 	new file:   media/quiz/DS_Quiz.xlsx 	new file:   media/quiz/DS_Quiz_vhSGsIF.xlsx 	modified:   quiz/__pycache__/admin.cpython-311.pyc 	modified:   quiz/__pycache__/models.cpython-311.pyc 	new file:   quiz/__pycache__/urls.cpython-311.pyc 	new file:   quiz/__pycache__/views.cpython-311.pyc 	modified:   quiz/admin.py 	new file:   quiz/migrations/0001_initial.py 	new file:   quiz/migrations/0002_alter_category_options_alter_quiz_options.py 	new file:   quiz/migrations/0003_rename_file_quiz_quiz_file_alter_quiz_category.py 	new file:   quiz/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   quiz/migrations/__pycache__/0002_alter_category_options_alter_quiz_options.cpython-311.pyc 	new file:   quiz/migrations/__pycache__/0003_rename_file_quiz_quiz_file_alter_quiz_category.cpython-311.pyc 	modified:   quiz/models.py 	new file:   quiz/urls.py 	modified:   quiz/views.py 	deleted:    templates/all-quiz.html 	new file:   templates/all_quiz.html 	modified:   templates/components/navbar.html 	modified:   templates/confirm.html 	modified:   templates/welcome.html

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+# import pandas as pd
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quiz_file = models.FileField(upload_to='quiz/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+    def __str__(self):
+        return self.title
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from account.models import Profile
+from .models import Quiz,Category
 
 # Create your views here.
+@login_required(login_url='login')
+def all_quiz_view(request):
+    user_object=User.objects.get(username=request.user)
+    user_profile=Profile.objects.get(user=user_object)
+
+    quizzes=Quiz.objects.order_by('-created_at')
+    categories=Category.objects.all()
+
+    context={"user_profile":user_profile,"quizzes":quizzes,"categories":categories}
+    return render(request, 'all_quiz.html', context)
+
+
+@login_required(login_url='login')
+def search_view(request,category):
+    user_object=User.objects.get(username=request.user)
+    user_profile=Profile.objects.get(user=user_object)
+
+    if category!="":
+        quizzes=Quiz.objects.filter(category__name=category)
+    else:
+        quizzes=Quiz.objects.order_by('-created_at')
+        
+    categories=Category.objects.all()
+
+    context={}
+    context={"user_profile":user_profile,"quizzes":quizzes,"categories":categories}
+    return render(request,'all_quiz.html',context)
