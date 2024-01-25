ddd4e32abdc2875ff8a43f2ab31b5880da875eb4
Anish Kumar Thakur <anishthakur3dec@gmail.com>
2024-01-25 21:52:53 +0530

    Changes to be committed: 	modified:   db.sqlite3 	new file:   media/quiz/DS_Quiz_OBc10qG.xlsx 	new file:   media/quiz/TestQuiz.xlsx 	new file:   media/quiz/WebQuiz5Qn.xlsx 	new file:   media/quiz/WebQuiz5Qn_W7Rjh1Z.xlsx 	modified:   quiz/__pycache__/admin.cpython-311.pyc 	modified:   quiz/__pycache__/models.cpython-311.pyc 	modified:   quiz/__pycache__/urls.cpython-311.pyc 	modified:   quiz/__pycache__/views.cpython-311.pyc 	modified:   quiz/admin.py 	new file:   quiz/migrations/0004_question_choice.py 	new file:   quiz/migrations/__pycache__/0004_question_choice.cpython-311.pyc 	modified:   quiz/models.py 	modified:   quiz/urls.py 	modified:   quiz/views.py 	modified:   requirement.txt 	modified:   templates/all_quiz.html 	modified:   templates/components/navbar.html 	modified:   templates/quiz.html 	modified:   templates/welcome.html

--- quiz/models.py
+++ quiz/models.py
@@ -1,12 +1,13 @@
+from collections.abc import Iterable
 from django.db import models
-# import pandas as pd
+import pandas as pd
 from django.contrib.auth.models import User
 from django.db.models import Sum
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-# Create your models here.
+#. your models here.
 class Category(models.Model):
     name = models.CharField(max_length=15)
 
@@ -29,3 +30,51 @@ class Quiz(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # call the function on quiz save
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+        if self.quiz_file:
+            self.import_quiz_from_excel()
+
+    def import_quiz_from_excel(self):
+        # read the excel file
+        df=pd.read_excel(self.quiz_file.path)
+
+        # iterate over the each row
+        for index,row in df.iterrows():
+            # extract question text, choices and the ccorrect answer from the row
+            question_text=row['Question']
+            choice1=row['A']
+            choice2=row['B']
+            choice3=row['C']
+            choice4=row['D']
+            correct_answer=row['Answer']
+
+            # create the questin object
+            question=Question.objects.get_or_create(quiz=self,text=question_text)
+
+
+            # create choices objects
+            choice_1=Choice.objects.get_or_create(question=question[0],text=choice1,is_correct=correct_answer=='A')
+            choice_2=Choice.objects.get_or_create(question=question[0],text=choice2,is_correct=correct_answer=='B')
+            choice_4=Choice.objects.get_or_create(question=question[0],text=choice4,is_correct=correct_answer=='D')
+            choice_3=Choice.objects.get_or_create(question=question[0],text=choice3,is_correct=correct_answer=='C')
+    
+
+class Question(models.Model):
+    quiz=models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    text=models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
+    
+class Choice(models.Model):
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)
+    text=models.CharField(max_length=255)
+    is_correct=models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.question.text[:50]} {self.text[:20]}"
+    
+    
--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from account.models import Profile
 from .models import Quiz,Category
+from django.db.models import Q
 
 # Create your views here.
 @login_required(login_url='login')
@@ -21,8 +22,15 @@ def all_quiz_view(request):
 def search_view(request,category):
     user_object=User.objects.get(username=request.user)
     user_profile=Profile.objects.get(user=user_object)
+    # search by search bar
 
-    if category!="":
+    if request.GET.get('q') != None:
+        q = request.GET.get('q')
+        query = Q(title__icontains=q) | Q(description__icontains=q)
+        quizzes = Quiz.objects.filter(query).order_by('-created_at')
+
+    # search by category
+    elif category!=" ":
         quizzes=Quiz.objects.filter(category__name=category)
     else:
         quizzes=Quiz.objects.order_by('-created_at')
@@ -32,3 +40,18 @@ def search_view(request,category):
     context={}
     context={"user_profile":user_profile,"quizzes":quizzes,"categories":categories}
     return render(request,'all_quiz.html',context)
+
+@login_required(login_url='login')
+def quiz_view(request,quiz_id):
+    user_object=User.objects.get(username=request.user)
+    user_profile=Profile.objects.get(user=user_object)
+
+
+    quiz=Quiz.objects.filter(id=quiz_id).first()
+    if quiz!=None:
+        context={"user_profile":user_profile,"quiz":quiz}
+    else:
+        return redirect('all_quiz')
+
+    
+    return render(request,'quiz.html',context)
