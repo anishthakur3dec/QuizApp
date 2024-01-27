76b680c88b052cd105248b783db1b03249a56d42
Anish Kumar Thakur <anishthakur3dec@gmail.com>
2024-01-27 21:51:18 +0530

    Changes to be committed: 	modified:   base/__pycache__/models.cpython-311.pyc 	modified:   base/__pycache__/urls.cpython-311.pyc 	modified:   base/__pycache__/views.cpython-311.pyc 	modified:   base/urls.py 	modified:   base/views.py 	modified:   db.sqlite3 	modified:   finalSubmission/__pycache__/settings.cpython-311.pyc 	modified:   finalSubmission/settings.py 	modified:   quiz/__pycache__/views.cpython-311.pyc 	modified:   quiz/views.py 	modified:   templates/components/navbar.html 	modified:   templates/leaderboard.html 	modified:   templates/quiz.html 	modified:   templates/welcome.html

--- base/views.py
+++ base/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.models import User
 from account.models import Profile
+from quiz.models import UserRank
+from django.contrib.auth.decorators import login_required
 
 def home(request):
+
+    leaderboard_user=UserRank.objects.order_by('rank')[:4]
+    
     if request.user.is_authenticated:
         user_object = User.objects.get(username=request.user)
         user_profile = Profile.objects.get(user=user_object)
         context = {"user_profile": user_profile}
         
     else:
-        context={}
+        context={"leaderboard_user":leaderboard_user}
         
     return render(request, 'welcome.html', context)
+
+@login_required(login_url="login")
+def leaderboard_view(request):
+
+    leaderboard_user=UserRank.objects.order_by('rank')
+
+    user_object = User.objects.get(username=request.user)
+    user_profile = Profile.objects.get(user=user_object)
+
+    context={"leaderboard_user":leaderboard_user,"user_profile":user_profile}
+    return render(request,"leaderboard.html",context)
--- finalSubmission/settings.py
+++ finalSubmission/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'account',
     'base',
     'quiz',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
